aee78042cfea209456e0baaf92518cd8a2b2f696
sigeharu <sigeharumiyamoto@gmail.com>
2020-07-19 02:33:30 +0000

    Finish signup_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id]) # ユーザーモデルからidを取り出す= User.find(1)みたいな状況
   end
+  
+  def new
+    @user = User.new # ユーザーオブジェクトを作成し、インスタンス変数に代入します
+  end
+  
+  def create
+    @user = User.new(user_params) # Userモデルのnewにuser_paramsの値を受け取り、@userに代入する
+    if @user.save #新しく挿入されたインスタンス係数@userを保存する
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user # railsではredirect_to user_url(@user)をredirect_to @userに簡略化できる
+    else
+      render :new #newアクションへ移動
+    end
+  end
+
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation) # 許可されたカラムのみが含まれたparamsハッシュ
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   
   get '/signup', to: 'users#new' # signup_path,signup_urlヘルパーでusers#newのコントローラの処理を行って/signupのURLをGETする
+  resources :users # 基本的にこの一行でusers_controllerをカバーできる
 end
